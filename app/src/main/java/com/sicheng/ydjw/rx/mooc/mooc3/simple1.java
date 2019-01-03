919e4a6adffff5a126d7ad87e1dfba292113a061
wangjingyun <1299880641@qq.com>
2019-01-03 20:44:00 +0800

    rxjava2 背压问题学习

---
+++ app/src/main/java/com/sicheng/ydjw/rx/mooc/mooc3/simple1.java
@@ -0,0 +1,66 @@
+package com.sicheng.ydjw.rx.mooc.mooc3;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
+import io.reactivex.schedulers.Schedulers;
+
+public class simple1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+                    @Override
+                    public void subscribe(FlowableEmitter<Integer> e) throws Exception {
+                        int i = 0;
+                        while (true) {
+                            //e.requested() 代表缓存池中可放入的数量 0的话缓存池中不能放入数据丢失超出根据缓存策略处理
+
+                            if (e.requested() == 0) continue;//此处添加代码，让flowable按需发送数据
+                            System.out.println("发射---->" + i);
+                            i++;
+                            e.onNext(i);
+                        }
+                    }
+                }, BackpressureStrategy.MISSING)//缓存策略抛异常处理
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(Schedulers.newThread())
+                .subscribe(new Subscriber<Integer>() {
+                    private Subscription mSubscription;
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        s.request(1);            //设置初始请求数据量为1
+                        mSubscription = s;
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        try {
+                            Thread.sleep(50);
+                            System.out.println("接收------>" + integer);
+                            mSubscription.request(1);//每接收到一条数据增加一条请求量
+                        } catch (InterruptedException ignore) {
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                    }
+
+                    @Override
+                    public void onComplete() {
+                    }
+                });
+    }
+}
