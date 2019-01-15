df0b41bd204479b1ca3b890e0e4e5f1fb853e5a3
wangjingyun <1299880641@qq.com>
2019-01-15 18:55:39 +0800

    修改

--- app/src/main/java/com/sicheng/ydjw/rx/mooc2/simple5.java
+++ app/src/main/java/com/sicheng/ydjw/rx/mooc2/simple5.java
@@ -1,8 +1,31 @@
 package com.sicheng.ydjw.rx.mooc2;
 
+import android.util.Log;
+
+import io.reactivex.functions.Consumer;
+import io.reactivex.subjects.PublishSubject;
+
 /**
- * 简化版 Observable
+ * 简化版 Observable  rxjava2  回调接口
  */
 public class simple5 {
    // Single、Completable、Maybe——简化版的Observable
+
+    public static void main(String args[]) {
+
+
+        //简易版 观察者模式
+        PublishSubject<String> publishSubject=PublishSubject.create();
+
+
+        publishSubject.subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+               System.out.print(s);
+            }
+        });
+
+
+        publishSubject.onNext("123456");
+    }
 }
